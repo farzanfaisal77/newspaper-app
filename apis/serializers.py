eacbbfe2bfb87a1b122d7f3665b863781314bb29
farzanfaisal77 <farzanfaisal77@gmail.com>
2026-06-11 08:28:14 +0530

    userserializer add (more to do)

--- apis/serializers.py
+++ apis/serializers.py
@@ -1,1 +1,9 @@
 from rest_framework import serializers
+from accounts.models import CustomUser
+from articles.models import Comment
+from articles.models import Article
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=CustomUser
+        fields=("id", "username", "email", "age",)
