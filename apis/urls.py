ece26a8120395c4c6c5952145c22a28ce7c621d2
farzanfaisal77 <farzanfaisal77@gmail.com>
2026-07-04 14:16:13 +0530

    fixing some api url bugs

--- apis/urls.py
+++ apis/urls.py
@@ -4,16 +4,16 @@ from drf_spectacular.views import SpectacularAPIView,SpectacularSwaggerView,Spec
 
 
 urlpatterns = [
-    path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("schema/redoc/", SpectacularRedocView.as_view(url_name="schema")),
-    path("schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema")),
+    path("schema/", SpectacularAPIView.as_view(), name="api_schema"),
+    path("schema/redoc/", SpectacularRedocView.as_view(url_name="api_schema")),
+    path("schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="api_schema")),
     path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
     path("dj-rest-auth/", include("dj_rest_auth.urls")),
-    path("users/<int:pk>/",UserDetail.as_view(), name="user_detail"),
-    path("users/",UserList.as_view(), name="user_list"),
-    path("articles/<int:article_pk>/comments/<int:comment_pk>/", CommentDetail.as_view(), name="comment_detail"),
-    path("articles/<int:article_pk>/comments/", CommentList.as_view(), name="comment_list"),
-    path("articles/<int:pk>/", ArticleDetail.as_view(), name="article_detail"),
-    path("articles/",ArticleList.as_view(), name="article_list"),
-    path("", HomeView.as_view(), name="apihome")
+    path("users/<int:pk>/",UserDetail.as_view(), name="api_user_detail"),
+    path("users/",UserList.as_view(), name="api_user_list"),
+    path("articles/<int:article_pk>/comments/<int:comment_pk>/", CommentDetail.as_view(), name="api_comment_detail"),
+    path("articles/<int:article_pk>/comments/", CommentList.as_view(), name="api_comment_list"),
+    path("articles/<int:pk>/", ArticleDetail.as_view(), name="api_article_detail"),
+    path("articles/",ArticleList.as_view(), name="api_article_list"),
+    path("", HomeView.as_view(), name="api_apihome")
 ]
